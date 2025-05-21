7c37ed436010f1f8ef6f9b4cbe768e9123773e53
Matias Sanchez <matias.fss97@gmail.com>
2025-05-21 17:05:51 -0400

    corregir error de card proyectos en Presentacion.jsx

--- src/components/Presentacion/Cards/CardProyectos.jsx
+++ src/components/Presentacion/Cards/CardProyectos.jsx
@@ -3,7 +3,7 @@
 export const CardProyectos = ({nombre, descripcion, children, url}) => {
   return (
     <>
-      <div className="mt-2">
+      <div className="mt-2" style={{ minHeight: "126px" }}>
         <h5 className="card-title">{nombre}</h5>
         <p className="card-text">{descripcion}</p>
         <a href={url} target="_blank" rel="noopener noreferrer">
